a1885edca417a3857de891546dd99992971cde48
jleldridge <jleldridge27@gmail.com>
2013-09-03 18:11:59 -0400

    Added comments to samples.

--- game/src/pyglet_samples/GettingInput.py
+++ game/src/pyglet_samples/GettingInput.py
@@ -1,15 +1,19 @@
 import pyglet
 from pyglet.window import key
 
+# instantiate the window
 window = pyglet.window.Window()
 
+# run this function every time window.event is called
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.A:
         print('The A key was pressed')
 
+# run this function every time window.event is called
 @window.event
 def on_draw():
     window.clear()
-    
+
+# start pyglet    
 pyglet.app.run()
--- game/src/pyglet_samples/ImageViewerSample.py
+++ game/src/pyglet_samples/ImageViewerSample.py
@@ -1,11 +1,16 @@
 import pyglet
 
+# instantiate the pyglet window
 window = pyglet.window.Window()
+
+# load an image
 image = pyglet.resource.image('Bouncer.jpg')
 
+# call this function every time an event takes place
 @window.event
 def on_draw():
     window.clear()
     image.blit(0, 0)
 
+# start pyglet
 pyglet.app.run()
