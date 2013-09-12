be6ca0e5b4c9d75ab1d8ffc2cb331428c43d906f
jleldridge <jleldridge27@gmail.com>
2013-09-12 11:55:30 -0400

    Worked on controller, not working as intended yet.

--- game/src/Main.py
+++ game/src/Main.py
@@ -4,16 +4,26 @@ SCREEN_X, SCREEN_Y = 800, 600
 
 # instantiate the window
 window = pyglet.window.Window(SCREEN_X, SCREEN_Y)
+keys_down = {'A': False, 'D': False}
+
+# these don't seem to work
+#keys = pyglet.window.key.KeyStateHandler()
+#window.push_handlers(keys)
 
 @window.event
 def on_key_press(symbol, modifiers):
-    control.controller.on_key_press(symbol, modifiers)
+    control.controller.on_key_press(symbol, keys_down, modifiers)
 
+@window.event
+def on_key_release(symbol, modifiers):
+    control.controller.on_key_release(symbol, keys_down, modifiers)
+    
 @window.event
 def on_draw():
     window.clear()
     
     # update and render the game
+    control.controller.handle_input(keys_down)
     control.engine.update()
     view.renderer.render(window)
 
--- game/src/control/Controller.py
+++ game/src/control/Controller.py
@@ -1,5 +1,24 @@
+import pyglet
 from pyglet.window import key
 
-def on_key_press(symbol, modifiers):
-    pass
+def on_key_press(symbol, keys_down, modifiers):
+    if symbol == key.A:
+        keys_down['A'] = True
+    elif symbol == key.D:
+        keys_Down['D'] = True
     
+def on_key_release(symbol, keys_down, modifiers):
+    if symbol == key.A:
+        keys_down['A'] = False
+    elif symbol == key.D:
+        keys_Down['D'] = False
+    
+def handle_input(keys_down):
+    if keys_down['A']:
+        print("A is pressed")
+    if keys_down['D']:
+        print("D is pressed")
+    # if keys_down[key.W]:
+        # print("W is pressed")
+    # if keys_down[key.S]:
+        # print("S is pressed")
