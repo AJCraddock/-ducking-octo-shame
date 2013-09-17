b1ecff9b185c1ce2b0ba7e23f44e57a83e7e2c3d
Andre <andre@Andre-Satellite-M305.(none)>
2013-09-16 21:00:59 -0400

    Player added

---
+++ game/src/model/player.py
@@ -0,0 +1,16 @@
+import pyglet
+class Player:
+        'player object'   
+        def __init__(self)
+            self.xcoordinate 
+            self.ycoordinate  
+            self.MAX_DX = 5
+            self.MAX_DY = 5
+            self.dx
+            self.dy
+        
+        def draw(self)
+            pyglet.graphics.draw(4,pyglet,gl.GL_POLYGON, ('v2i', (10, 15, 30, 35)))
+            
+            
+             
