871755f277e901212b62bc71c5c249fd59a017ac
jleldridge <jleldridge27@gmail.com>
2013-09-27 14:29:00 -0400

    Changed grid initialization to empty list.

--- game/src/model/map.py
+++ game/src/model/map.py
@@ -1,6 +1,6 @@
 
 class Map:
     def __init__(self, player, game_entities, grid_width, grid_height):
-        self.grid = [[None for i in range(grid_width)] for j in range[grid_height]]
+        self.grid = [[[] for i in range(grid_width)] for j in range[grid_height]]
         self.player = player
         self.game_entities = game_entities
