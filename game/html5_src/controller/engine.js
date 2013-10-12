fcb75a1c04c3fe46bce01e7ccbcc65bf9efe9215
jleldridge <jleldridge27@gmail.com>
2013-10-12 19:21:37 -0400

    Created a map loader to create the maps for the engine.

--- game/html5_src/controller/engine.js
+++ game/html5_src/controller/engine.js
@@ -1,16 +1,15 @@
 function Engine(player_controller, map){
     var GRAVITY = 0.3;
-    var player_controller = player_controller;
+    this.player_controller = player_controller;
     this.map = map;
-    var player = map.player;
 
     this.update = function(){
-        player_controller.handle_input(player);
-        gravity(player);
-        map.player.x += map.player.dx;
-        map.player.y += map.player.dy;
-        map.player.on_ground = false;
-        //check for player collisions
+        this.player_controller.handle_input(this.map.player);
+        gravity(this.map.player);
+        this.map.player.x += this.map.player.dx;
+        this.map.player.y += this.map.player.dy;
+        this.map.player.on_ground = false;
+        //check forthis.map.player collisions
         for(var i = 0; i < map.objects.length; i++){
             var o = map.objects[i];
             var collision = check_player_collision(o);
@@ -23,43 +22,43 @@ function Engine(player_controller, map){
     var handle_player_collision = function(o){
         var temp_x, temp_y, temp_dx, temp_dy;
         var temp_on_ground;
-        //player is above object
-        if (player.y < o.y){
-            temp_y = o.y-player.height;
+        //this.player is above object
+        if (this.map.player.y < o.y){
+            temp_y = o.y-this.map.player.height;
             temp_on_ground = true;
             temp_dy = 0;
-        }else{ //player is below object
+        }else{ //this.player is below object
             temp_y = o.y+o.height
             temp_on_ground = false;
             temp_dy = 0;
         }
-        if (player.x < o.x){
-            temp_x = o.x-player.width;
+        if (this.map.player.x < o.x){
+            temp_x = o.x-this.map.player.width;
             temp_dx = 0;
         }else{
             temp_x = o.x+o.width;
             temp_dx = 0;
         }
         //check which overlap is greater
-        if(Math.abs(temp_x-player.x) < Math.abs(temp_y-player.y)){
-            player.x = temp_x;
-            player.dx = temp_dx;
+        if(Math.abs(temp_x-this.map.player.x) < Math.abs(temp_y-this.map.player.y)){
+            this.map.player.x = temp_x;
+            this.map.player.dx = temp_dx;
         }else{
-            player.y = temp_y;
-            player.on_ground = temp_on_ground;
-            player.dy = temp_dy;
+            this.map.player.y = temp_y;
+            this.map.player.on_ground = temp_on_ground;
+            this.map.player.dy = temp_dy;
         }
     };
 
     var check_player_collision = function(object){
         //check if there is no overlap on x axis.
-        if (player.x+player.width < object.x || 
-            player.x > object.x+object.width){
+        if (this.map.player.x+this.map.player.width < object.x || 
+            this.map.player.x > object.x+object.width){
             return false;
         }
         //check if there is no overlap on y axis.
-        else if(player.y+player.height < object.y || 
-            player.y > object.y+object.height){
+        else if(this.map.player.y+this.map.player.height < object.y || 
+            this.map.player.y > object.y+object.height){
             return false;
         }
         //if both checks fail, both axes overlap and there is a collision.
