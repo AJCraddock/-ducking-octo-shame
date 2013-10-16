6721f47bb688c362d734f5ddf4a9c921ad3c9f83
jleldridge <jleldridge27@gmail.com>
2013-10-16 16:32:44 -0400

    Rewrote map and maploader.

--- game/html5_src/controller/map_loader.js
+++ game/html5_src/controller/map_loader.js
@@ -1,48 +1,69 @@
-//imports
-var head = document.getElementsByTagName('head')[0];
-var mapjs = document.createElement('script');
-mapjs.type = 'text/javascript';
-mapjs.src = '../model/map.js';
-head.appendChild(mapjs);
+define(function(require){
+    //constructor
+    function MapLoader(){
+        //fields
+        this.curr_map_index = 0;
+        this.maps = new Array();
 
-function MapLoader(){
-    //fields
-    this.curr_map_index = 0;
-    this.maps = new Array();
+        //temporary map declarations
+        var background = document.createElement("canvas");
+        background.width = 800;
+        background.height = 600;
+        var temp_graphics = background.getContext('2d');
+        temp_graphics.fillStyle = "#000000";
+        temp_graphics.fillRect(0, 0, background.width, background.height);
+        var player = new Player(80, 80);
+        var objects = new Array();
+        objects.push(new StaticObject(0, 525, 600, 10));
+        objects.push(new StaticObject(400, 425, 90, 100));
+        objects.push(new StaticObject(200, 489, 90, 10));
 
-    //temporary map declarations
-    var background = document.createElement("canvas");
-    background.width = 800;
-    background.height = 600;
-    var temp_graphics = background.getContext('2d');
-    temp_graphics.fillStyle = "#000000";
-    temp_graphics.fillRect(0, 0, background.width, background.height);
-    var player = new Player(80, 80);
-    var objects = new Array();
-    objects.push(new StaticObject(0, 525, 600, 10));
-    objects.push(new StaticObject(400, 425, 90, 100));
-    objects.push(new StaticObject(200, 489, 90, 10));
-    
-    this.map01 = new Map(player, objects, background);
-    this.maps.push(this.map01);
-    
-    // var background = document.createElement("canvas");
-    // background.width = 800;
-    // background.height = 600;
-    // var temp_graphics = background.getContext('2d');
-    // temp_graphics.fillStyle = "#000000";
-    // temp_graphics.fillRect(0, 0, background.width, background.height);
-    // var player = new Player(80, 80);
-    // var objects = new Array();
-    // objects.push(new StaticObject(0, 525, 600, 10));
-    // objects.push(new StaticObject(100, 425, 90, 100));
-    // objects.push(new StaticObject(500, 489, 90, 10));
+        this.map01 = new Map(player, objects, background);
+        this.maps.push(this.map01);
+    }
 
-    // this.map02 = new Map(player, objects, background);
-    // this.maps.push(this.map02);
+    //superclass definition
+    MapLoader.prototype = {
+        constructor: MapLoader,
 
-    this.load_next_map = function(){
-        this.curr_map_index = (this.curr_map_index+1)%this.maps.length;
-        return this.maps[this.curr_map_index];
+        load_next_map: function(){
+            this.curr_map_index = (this.curr_map_index+1)%this.maps.length;
+            return this.maps[this.curr_map_index];
+        }
     };
-}
+});
+
+//OLD CODE
+// //imports
+// var head = document.getElementsByTagName('head')[0];
+// var mapjs = document.createElement('script');
+// mapjs.type = 'text/javascript';
+// mapjs.src = '../model/map.js';
+// head.appendChild(mapjs);
+
+// function MapLoader(){
+//     //fields
+//     this.curr_map_index = 0;
+//     this.maps = new Array();
+
+//     //temporary map declarations
+//     var background = document.createElement("canvas");
+//     background.width = 800;
+//     background.height = 600;
+//     var temp_graphics = background.getContext('2d');
+//     temp_graphics.fillStyle = "#000000";
+//     temp_graphics.fillRect(0, 0, background.width, background.height);
+//     var player = new Player(80, 80);
+//     var objects = new Array();
+//     objects.push(new StaticObject(0, 525, 600, 10));
+//     objects.push(new StaticObject(400, 425, 90, 100));
+//     objects.push(new StaticObject(200, 489, 90, 10));
+    
+//     this.map01 = new Map(player, objects, background);
+//     this.maps.push(this.map01);
+
+//     this.load_next_map = function(){
+//         this.curr_map_index = (this.curr_map_index+1)%this.maps.length;
+//         return this.maps[this.curr_map_index];
+//     };
+// }
--- game/html5_src/model/map.js
+++ game/html5_src/model/map.js
@@ -1,5 +1,21 @@
-function Map(player, objects, background){
-    this.player = player;
-    this.objects = objects;
-    this.background = background;
-}
+define(function(require){
+    //constructor
+    function Map(player, objects, background){
+        this.player = player;
+        this.objects = objects;
+        this.background = background; 
+    }
+
+    //superclass definition
+    Map.prototype = {
+        constructor: Map
+    };
+});
+
+
+//OLD CODE
+// function Map(player, objects, background){
+//     this.player = player;
+//     this.objects = objects;
+//     this.background = background;
+// }
