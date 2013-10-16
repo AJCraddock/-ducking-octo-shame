2c0f14f3906f3bf618608da65dd74a001a2da452
jleldridge <jleldridge27@gmail.com>
2013-10-16 17:18:25 -0400

    Rewrote player_controller.

--- game/html5_src/controller/player_controller.js
+++ game/html5_src/controller/player_controller.js
@@ -1,45 +1,105 @@
-function PlayerController(){
-    this.keys_down = {A:false, D:false, W:false, S:false, Space:false};
+define(
+    //dependencies
+    [],
 
-    this.on_keydown = function(event){
-        var key = event.keyCode;
-        if (key == KeyboardEvent.DOM_VK_A){
-            this.keys_down.A = true;
-        }else if(key == KeyboardEvent.DOM_VK_D){
-            this.keys_down.D = true;
-        }else if(key == KeyboardEvent.DOM_VK_SPACE){
-            this.keys_down.Space = true;
+    //module definition
+    function(){
+        function PlayerController(){
+            this.keys_down = {A:false, D:false, W:false, S:false, Space:false};            
         }
-    };
 
-    this.on_keyup = function(event){
-        var key = event.keyCode;
-        if (key == KeyboardEvent.DOM_VK_A){
-            this.keys_down.A = false;
-        }else if(key == KeyboardEvent.DOM_VK_D){
-            this.keys_down.D = false;
-        }else if(key == KeyboardEvent.DOM_VK_SPACE){
-            this.keys_down.Space = false;
-        }
-    };
+        //superclass definition
+        PlayerController.prototype = {
+            constructor: PlayerController,
 
-    this.handle_input = function(player){
-        if (this.keys_down.A){
-            player.dx = 0-player.MAX_DX;
-        }else if(this.keys_down.D){
-            player.dx = player.MAX_DX;
-        }else{
-            player.dx = 0;
-        }
-        if (this.keys_down.Space){
-            this.jump(player);
-        }
-    };
+            on_keydown: function(event){
+                var key = event.keyCode;
+                if (key == KeyboardEvent.DOM_VK_A){
+                    this.keys_down.A = true;
+                }else if(key == KeyboardEvent.DOM_VK_D){
+                    this.keys_down.D = true;
+                }else if(key == KeyboardEvent.DOM_VK_SPACE){
+                    this.keys_down.Space = true;
+                }
+            },
 
-    this.jump = function(player){
-        if (player.on_ground){
-            player.dy = 0-player.MAX_DY;
-            player.on_ground = false;
-        }
-    };
-}
+            on_keyup: function(event){
+                var key = event.keyCode;
+                if (key == KeyboardEvent.DOM_VK_A){
+                    this.keys_down.A = false;
+                }else if(key == KeyboardEvent.DOM_VK_D){
+                    this.keys_down.D = false;
+                }else if(key == KeyboardEvent.DOM_VK_SPACE){
+                    this.keys_down.Space = false;
+                }
+            },
+
+            handle_input: function(player){
+                if (this.keys_down.A){
+                    player.dx = 0-player.MAX_DX;
+                }else if(this.keys_down.D){
+                    player.dx = player.MAX_DX;
+                }else{
+                    player.dx = 0;
+                }
+                if (this.keys_down.Space){
+                    this.jump(player);
+                }
+            },
+
+            jump: function(player){
+                if (player.on_ground){
+                    player.dy = 0-player.MAX_DY;
+                    player.on_ground = false;
+                }
+            }
+        };
+    }
+);
+
+//OLD CODE
+// function PlayerController(){
+//     this.keys_down = {A:false, D:false, W:false, S:false, Space:false};
+
+//     this.on_keydown = function(event){
+//         var key = event.keyCode;
+//         if (key == KeyboardEvent.DOM_VK_A){
+//             this.keys_down.A = true;
+//         }else if(key == KeyboardEvent.DOM_VK_D){
+//             this.keys_down.D = true;
+//         }else if(key == KeyboardEvent.DOM_VK_SPACE){
+//             this.keys_down.Space = true;
+//         }
+//     };
+
+//     this.on_keyup = function(event){
+//         var key = event.keyCode;
+//         if (key == KeyboardEvent.DOM_VK_A){
+//             this.keys_down.A = false;
+//         }else if(key == KeyboardEvent.DOM_VK_D){
+//             this.keys_down.D = false;
+//         }else if(key == KeyboardEvent.DOM_VK_SPACE){
+//             this.keys_down.Space = false;
+//         }
+//     };
+
+//     this.handle_input = function(player){
+//         if (this.keys_down.A){
+//             player.dx = 0-player.MAX_DX;
+//         }else if(this.keys_down.D){
+//             player.dx = player.MAX_DX;
+//         }else{
+//             player.dx = 0;
+//         }
+//         if (this.keys_down.Space){
+//             this.jump(player);
+//         }
+//     };
+
+//     this.jump = function(player){
+//         if (player.on_ground){
+//             player.dy = 0-player.MAX_DY;
+//             player.on_ground = false;
+//         }
+//     };
+// }
