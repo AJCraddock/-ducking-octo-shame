9c2fb6d953d110a5067461a65964d55136b6bca8
Luke Eldridge <jleldrid@uncg.edu>
2013-10-17 23:49:33 -0400

    Allowed the main_canvas to get focus by clicking on it.

--- game/html5_src/main.js
+++ game/html5_src/main.js
@@ -16,17 +16,27 @@ requirejs(
         //set up the canvas.
         var main_canvas = document.getElementById('main_canvas');
         var fore_canvas = document.getElementById('fore_canvas');
+        var volatile_canvas = document.getElementById('volatile_canvas');
         //give the main_canvas focus
         main_canvas.setAttribute('tabindex', '0');
         main_canvas.focus();
         //link event handlers for game
         main_canvas.addEventListener("keydown", on_keydown, false);
         main_canvas.addEventListener("keyup", on_keyup, false);
+        // event handlers to allow the main_canvas to get focus back
+        fore_canvas.addEventListener("focus", on_focus, false);
+        fore_canvas.addEventListener("mouseup", on_focus, false);
+        volatile_canvas.addEventListener("focus", on_focus, false);
+        volatile_canvas.addEventListener("mouseup", on_focus, false);
         //get graphics context for the canvas
         var back_graphics = main_canvas.getContext('2d');
         var graphics = fore_canvas.getContext('2d');
         //set the main canvas background to the current map background
         back_graphics.drawImage(map.background, 0, 0);
+
+        function on_focus(event){
+            main_canvas.focus();
+        }
 
         function on_keydown(event){
             engine.player_controller.on_keydown(event);
