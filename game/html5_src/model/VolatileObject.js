3d3b150f547716b5aed321f7e90ab3bf1f026c1d
Luke Eldridge <jleldrid@uncg.edu>
2013-10-23 20:34:53 -0400

    Added volatile_object class.

---
+++ game/html5_src/model/VolatileObject.js
@@ -0,0 +1,36 @@
+define(
+    //module definition
+    function(){
+        //constructor
+        function VolatileObject(x, y, width, height){
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        VolatileObject.prototype = {
+            constructor: VolatileObject,
+
+            //handle collisions in a special way
+            on_collide: function(obj){
+            },
+
+            clear_old: function(graphics){
+                graphics.clearRect(this.last_render_x-2, this.last_render_y-2, 
+                this.width+4, this.height+4);
+            },
+
+            draw: function(graphics, screen_x){
+                graphics.fillStyle = "#FFFFFF";
+                var object_draw_x = this.x-screen_x;
+                graphics.fillRect(object_draw_x, this.y, this.width, this.height);                
+                
+                this.last_render_x = object_draw_x;
+                this.last_render_y = this.y;
+            }
+        };
+
+        return StaticObject;
+    }
+);
