4007ab88fe36c66354e3ab559eacd8b17fbe3104
jleldridge <jleldridge27@gmail.com>
2013-10-24 18:27:11 -0400

    Created a goal platform that will change the state of the game to a victory state if touched.

---
+++ game/html5_src/model/GoalPlatform.js
@@ -0,0 +1,33 @@
+define(
+    //module definition
+    function(){
+        //dependencies
+        ['model/VolatileObject'],
+        //constructor
+        function GoalPlatform(x, y){
+            this.x = x;
+            this.y = y;
+            this.width = 80;
+            this.height = 20;
+        }
+
+        GoalPlatform.prototype = VolatileObject.prototype;
+
+        GoalPlatform.prototype.constructor = GoalPlatform;
+
+        GoalPlatform.prototype.on_collide = function(engine){
+            engine.victory = true;
+        };
+
+        GoalPlatform.prototype.draw = function(graphics, screen_x){
+            graphics.fillStyle = "#0000FF";
+            var object_draw_x = this.x-screen_x;
+            graphics.fillRect(object_draw_x, this.y, this.width, this.height);                
+            
+            this.last_render_x = object_draw_x;
+            this.last_render_y = this.y;
+        };
+
+        return GoalPlatform;
+    }
+);
