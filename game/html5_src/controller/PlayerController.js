5b2eb24508395950b33df8d4961825e41e1d10cd
Luke Eldridge <jleldrid@uncg.edu>
2013-10-30 23:01:22 -0400

    Created a controller superclass.

--- game/html5_src/controller/PlayerController.js
+++ game/html5_src/controller/PlayerController.js
@@ -1,61 +1,33 @@
 define(
     //dependencies
-    [],
+    ['controller/Controller'],
 
     //module definition
-    function(){
+    function(Controller){
         function PlayerController(){
-            this.keys_down = {A:false, D:false, W:false, S:false, Space:false};            
+            Controller.prototype.constructor.call(this);            
         }
 
-        var A = 65;
-        var D = 68;
-        var SPACE = 32;
+        PlayerController.prototype = new Controller();
+        PlayerController.prototype.constructor = PlayerController;
 
-        //superclass definition
-        PlayerController.prototype = {
-            constructor: PlayerController,
+        PlayerController.prototype.handle_input = function(player){
+            if (this.keys_down.A){
+                player.dx = 0-player.MAX_DX;
+            }else if(this.keys_down.D){
+                player.dx = player.MAX_DX;
+            }else{
+                player.dx = 0;
+            }
+            if (this.keys_down.Space){
+                this.jump(player);
+            }
+        };
 
-            on_keydown: function(event){
-                var key = event.keyCode;
-                if (key == A){
-                    this.keys_down.A = true;
-                }else if(key == D){
-                    this.keys_down.D = true;
-                }else if(key == SPACE){
-                    this.keys_down.Space = true;
-                }
-            },
-
-            on_keyup: function(event){
-                var key = event.keyCode;
-                if (key == A){
-                    this.keys_down.A = false;
-                }else if(key == D){
-                    this.keys_down.D = false;
-                }else if(key == SPACE){
-                    this.keys_down.Space = false;
-                }
-            },
-
-            handle_input: function(player){
-                if (this.keys_down.A){
-                    player.dx = 0-player.MAX_DX;
-                }else if(this.keys_down.D){
-                    player.dx = player.MAX_DX;
-                }else{
-                    player.dx = 0;
-                }
-                if (this.keys_down.Space){
-                    this.jump(player);
-                }
-            },
-
-            jump: function(player){
-                if (player.on_ground){
-                    player.dy = 0-player.MAX_DY;
-                    player.on_ground = false;
-                }
+        PlayerController.prototype.jump = function(player){
+            if (player.on_ground){
+                player.dy = 0-player.MAX_DY;
+                player.on_ground = false;
             }
         };
 
