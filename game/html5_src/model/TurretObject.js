276d953c3a04d80a97145e1b10e610f3c819164d
jleldridge <jleldridge27@gmail.com>
2013-11-04 11:11:49 -0500

    Changed name of turret class.

--- game/html5_src/model/TurretObject.js
+++ game/html5_src/model/TurretObject.js
@@ -5,18 +5,18 @@ define(
     //module definition
     function(){
         //constructor
-        function TurrentObject(x, y){
+        function TurretObject(x, y){
             this.x = x;
             this.y = y;
             this.width = 28;
             this.height = 28;
         }
 
-        TurrentObject.prototype = GameObject.prototype;
+        TurretObject.prototype = GameObject.prototype;
 
-        TurrentObject.prototype.constructor = TurrentObject;
+        TurretObject.prototype.constructor = TurretObject;
 
-        TurrentObject.prototype.draw = function(graphics, screen_x){
+        TurretObject.prototype.draw = function(graphics, screen_x){
             graphics.fillStyle = "#FF0000";
             var object_draw_x = this.x-screen_x;
             graphics.fillRect(object_draw_x, this.y, this.width, this.height);                
@@ -27,6 +27,6 @@ define(
         function Shoot(graphics, x){
             new Bullet(this.x-10, this.y-10);
         };
-        return TurrentObject;
+        return TurretObject;
     }
 );
