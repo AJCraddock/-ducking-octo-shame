0be17247398dbb35d6e6e47b397f5eb326da2919
jleldridge <jleldridge27@gmail.com>
2013-11-07 11:23:51 -0500

    Moved map json to its own file.

---
+++ game/html5_src/model/Maps.js
@@ -0,0 +1,67 @@
+define(
+    [],
+    function(){
+        Maps = 
+        [
+        {
+            "Player": {"x":80, "y":80},
+            "GameObjects":
+            [
+                {"type": "GameObject", "x":0, "y":0, "width":30, "height":600},
+                {"type":"GoalPlatform", "x":3500, "y":525},
+                {"type":"Mechanism", "x":31, "y":525, "width":300, "height":100, "cycles_to_goal":200, "goals":[
+                        {"x":31, "y":200}
+                    ]
+                },
+                {"type":"Mechanism", "x":400, "y":225, "width":90, "height":100, "cycles_to_goal":100, "goals":[
+                        {"x":400, "y":525}
+                    ]
+                },
+                {"type":"Mechanism", "x":700, "y":525, "width":600, "height":100, "cycles_to_goal":200, "goals":[
+                        {"x":700, "y":100}
+                    ]
+                },
+                {"type":"Mechanism", "x":1400, "y":100, "width":600, "height":10, "cycles_to_goal":200, "goals":[
+                        {"x":1400, "y":525}
+                    ]
+                },
+                {"type": "GameObject", "x":2100, "y":525, "width":600, "height":10},
+                {"type":"Mechanism", "x":2800, "y":525, "width":100, "height":10, "cycles_to_goal":200, "goals":[
+                        {"x":3300, "y":525}
+                    ]
+                },
+            ],
+            "Background": '#000000'
+        },
+        {
+            "Player": {"x":80, "y":80},
+            "GameObjects":
+            [
+                {"type":"GoalPlatform", "x":3500, "y":525},
+                {"type": "GameObject", "x":0, "y":525, "width":600, "height":10},
+                {"type":"GameObject", "x":0, "y":0, "width":30, "height":600},
+                {"type":"Mechanism", "x":400, "y":225, "width":90, "height":100, "cycles_to_goal":100, "goals":[
+                        {"x":400, "y":525}
+                    ]
+                },
+                {"type":"Mechanism", "x":700, "y":525, "width":600, "height":100, "cycles_to_goal":200, "goals":[
+                        {"x":700, "y":100}
+                    ]
+                },
+                {"type":"Mechanism", "x":1400, "y":100, "width":600, "height":10, "cycles_to_goal":200, "goals":[
+                        {"x":1400, "y":525}
+                    ]
+                },
+                {"type": "GameObject", "x":2100, "y":525, "width":600, "height":10},
+                {"type":"Mechanism", "x":2800, "y":525, "width":100, "height":10, "cycles_to_goal":200, "goals":[
+                        {"x":3300, "y":525}
+                    ]
+                },
+            ],
+            "Background": '#000000'
+        },
+        ];
+
+        return Maps;
+    }
+);
