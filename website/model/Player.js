904a981471784170b8446e60810da7a984f9d741
jleldridge <jleldridge27@gmail.com>
2013-11-22 11:47:17 -0500

    Made the website match the online version.

--- website/model/Player.js
+++ website/model/Player.js
@@ -10,7 +10,7 @@ define(
             this.y = y;
             this.dx = 0;
             this.dy = 0;
-            this.ddx = 0.10;
+            this.ddx = 0.20;
             this.MAX_DX = 5;
             this.MAX_DY = 10;
             this.width = 25;
